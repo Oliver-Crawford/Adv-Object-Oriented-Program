548d18c8848e669a8b6b52b290ad0b2b36a45a94
godzilla <godzilla@BB21106>
2024-01-25 17:35:57 -0600

    Add project files.

---
+++ Adv Object-Oriented Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv_Object_Oriented_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
