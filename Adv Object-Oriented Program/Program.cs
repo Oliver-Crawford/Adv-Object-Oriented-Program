997c01c12df4fecca79717233cddae5d512cd808
Oliver Crawford <oicrawford@gmail.com>
2024-01-25 18:47:21 -0600

    Work in class on 1/25/2024

--- Adv Object-Oriented Program/Program.cs
+++ Adv Object-Oriented Program/Program.cs
@@ -6,11 +6,47 @@ using System.Threading.Tasks;
 
 namespace Adv_Object_Oriented_Program
 {
+    class MyClass
+    {
+        // Class members
+        //Default values
+        public string color = "red";        // public field
+        private int maxSpeed = 200;          // private field (default if not declared)
+        private string encapsulation;
+        public MyClass()
+        {
+
+        }
+
+        public MyClass(string colorInit, int maxSpeedInit)
+        {
+            color = colorInit;
+            maxSpeed = maxSpeedInit;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine($"Going {maxSpeed}MPH!");
+        }
+    }
+        
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("TEST");
+            //create a new class, needs the initializing info
+            MyClass withinits = new MyClass("Blue", 10);
+            MyClass withouinits = new MyClass();
+            //can call objects labeled public, if not declared public it isn't in scope
+            Console.WriteLine(withinits.color);
+            Console.WriteLine(withouinits.color);
+            //can't access objects such as .maxSpeed due to being private
+
+            //Also applies to methods
+            withinits.fullThrottle();
+
+            
+            Console.ReadLine();
         }
     }
+    
 }
